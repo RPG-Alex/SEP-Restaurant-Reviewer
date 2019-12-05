fce08ad7c5cc3f7c89b019cabd611722d68cdfe0
Alex <mralexblood@outlook.com>
2019-12-05 15:34:12 +0800

    started working on the review posting view. Not finished, just a concept

---
+++ view/postReview.php
@@ -0,0 +1,120 @@
+
+    <img src="https://img.pngio.com/example-stamp-png-graphic-black-and-white-stock-brunei-vehicle-example-stamp-png-820_317.png" alt=""> <! !>
+    <h1>Restuarant Title (need to modify)</h1>
+    <hr>
+<form class="" action="" method="">
+  <table border>
+    <tr>
+      <td>Dining Aspect</td>
+      <td>1</td>
+      <td>2</td>
+      <td>3</td>
+      <td>4</td>
+      <td>5</td>
+      <td>6</td>
+      <td>7</td>
+      <td>8</td>
+      <td>9</td>
+      <td>10</td>
+    </tr>
+    <tr>
+      <td>Quality of Food</td>
+      <td>
+        <input type="radio" id="1">
+      </td>
+      <td>
+        <input type="radio" id="2">
+      </td>
+      <td>
+        <input type="radio" id="3">
+      </td>
+      <td>
+        <input type="radio" id="4">
+      </td>
+      <td>
+        <input type="radio" id="5">
+      </td>
+      <td>
+        <input type="radio" id="6">
+      </td>
+      <td>
+        <input type="radio" id="7">
+      </td>
+      <td>
+        <input type="radio" id="8">
+      </td>
+      <td>
+        <input type="radio" id="9">
+      </td>
+      <td>
+        <input type="radio" id="10">
+      </td>
+    </tr>
+    <tr>
+      <td>Service</td>
+      <td>
+        <input type="radio" id="1">
+      </td>
+      <td>
+        <input type="radio" id="2">
+      </td>
+      <td>
+        <input type="radio" id="3">
+      </td>
+      <td>
+        <input type="radio" id="4">
+      </td>
+      <td>
+        <input type="radio" id="5">
+      </td>
+      <td>
+        <input type="radio" id="6">
+      </td>
+      <td>
+        <input type="radio" id="7">
+      </td>
+      <td>
+        <input type="radio" id="8">
+      </td>
+      <td>
+        <input type="radio" id="9">
+      </td>
+      <td>
+        <input type="radio" id="10">
+      </td>
+    </tr>
+    <tr>
+      <td>Value</td>
+      <td>
+        <input type="radio" id="1">
+      </td>
+      <td>
+        <input type="radio" id="2">
+      </td>
+      <td>
+        <input type="radio" id="3">
+      </td>
+      <td>
+        <input type="radio" id="4">
+      </td>
+      <td>
+        <input type="radio" id="5">
+      </td>
+      <td>
+        <input type="radio" id="6">
+      </td>
+      <td>
+        <input type="radio" id="7">
+      </td>
+      <td>
+        <input type="radio" id="8">
+      </td>
+      <td>
+        <input type="radio" id="9">
+      </td>
+      <td>
+        <input type="radio" id="10">
+      </td>
+    </tr>
+  </table>
+</form>
