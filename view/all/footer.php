13992430a2eb9a415bc8112b20b265552a91d35e
nei1d0r <superman4kryptonite@hotmail.com>
2019-12-13 23:45:45 +0000

    Updated footer - basic

--- view/all/footer.php
+++ view/all/footer.php
@@ -1,5 +1,10 @@
-<footer>
-  <b>Copyright 2019 - <?php echo date('Y'); //returns current copyright range?></b> Alex Blood & Neil Davis
-</footer>
+<footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Resturaunt Reviewer</strong> by <a href="https://github.com/alexbteacher/SEP-Restaurant-Reviewer">Alex Blood & Neil Davis: 2019 <?php echo date('Y'); //returns current copyright range?></a>
+        </p>
+      </div>
+    </footer>
+
   </body>
 </html>
