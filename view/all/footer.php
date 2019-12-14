2326d2dd95e4607b35b8bc4d0d9f476765416d9d
Alex <mralexblood@outlook.com>
2019-12-14 10:15:32 +0800

    just updated the copyright so it looks clearer

--- view/all/footer.php
+++ view/all/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
       <div class="content has-text-centered">
         <p>
-          <strong>Resturaunt Reviewer</strong> by <a href="https://github.com/alexbteacher/SEP-Restaurant-Reviewer">Alex Blood & Neil Davis: 2019 <?php echo date('Y'); //returns current copyright range?></a>
+          <strong>Resturaunt Reviewer</strong> by <a href="https://github.com/alexbteacher/SEP-Restaurant-Reviewer">Alex Blood & Neil Davis: 2019 - <?php echo date('Y'); //returns current copyright range?></a>
         </p>
       </div>
     </footer>
