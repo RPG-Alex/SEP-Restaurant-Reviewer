b0654bd0b92ac2eeddb3b7156a6c813564fc30ea
Alex <mralexblood@outlook.com>
2019-12-14 10:08:15 +0800

    Fixed a minor bug where the seach value input by user was being output below seach bar, instead of back into the search bar

--- view/home.php
+++ view/home.php
@@ -2,7 +2,7 @@
 
 <section class="columns hero is-medium">
   <div class="hero-body level-left">
-    
+
     <div class="column container">
       <h1 class="title">
         City X's Restuarant Reviews!
@@ -11,27 +11,27 @@
         Brought to you by news Organization!
       </h2>
     </div>
-  
-  
+
+
     <div class=" field is-grouped container level-right">
 
       <form class="column" action='<?php echo 'index.php?page=search'; ?>' method="post">
 
         <div class="field">
           <div class="control is-expanded">
-            <input class="input is-large" type="text" name="search" placeholder="Where did you eat today?">
+            <input class="input is-large" type="text" name="search" placeholder="Where did you eat today?" <?php
+              //This uses get to check if it needs to reload a seach term. If a user navigates to page=search manually it will return not input value
+              if (isset($_GET['page'])){
+                if (isset($_POST['search'])) {
+                  echo "value='".$_POST['search']."'";
+                }
+              }
+            ?>>
           </div>
         </div>
 
 
-        <?php
-          //This uses get to check if it needs to reload a seach term. If a user navigates to page=search manually it will return not input value
-          if (isset($_GET['page'])){
-            if (isset($_POST['search'])) {
-              echo "value='".$_POST['search']."'";
-            }
-          }
-        ?>
+
         <div>
           <input class="button is-primary is-large is-outlined level-right" type="submit" name="submit" value="GO!">
         </div>
