5ffdadde71f0b0a946a0d36e304188ffe26d66eb
Neildor <superman4kryptonite@hotmail.com>
2019-12-21 15:54:05 +0000

    submitted search now submits to same  page this will make styling easier

--- view/home.php
+++ view/home.php
@@ -1,44 +1,47 @@
 <div class="container is-fluid centered-level">
 
-<section class="columns hero is-medium">
-  <div class="hero-body level-left">
+  <section class="columns hero is-medium">
+    <div class="hero-body level-left">
 
-    <div class="column container">
-      <h1 class="title">
-        City X's Restuarant Reviews!
-      </h1>
-      <h2 class="subtitle">
-        Brought to you by news Organization!
-      </h2>
-    </div>
+      <div class="column">
+        <h1 class="title">
+          City X's Restuarant Reviews!
+        </h1>
+        <h2 class="subtitle">
+          Brought to you by news Organization!
+        </h2>
+        <form class="column" action='<?php echo 'index.php?page=search'; ?>' method="post">
 
-
-    <div class=" field is-grouped container level-right">
-
-      <form class="column" action='<?php echo 'index.php?page=search'; ?>' method="post">
-
-        <div class="field">
-          <div class="control is-expanded">
-            <input class="input is-large" type="text" name="search" placeholder="Where did you eat today?" <?php
-              //This uses get to check if it needs to reload a seach term. If a user navigates to page=search manually it will return not input value
-              if (isset($_GET['page'])){
-                if (isset($_POST['search'])) {
-                  echo "value='".$_POST['search']."'";
+          <div class="field is-grouped">
+            <div class="control is-expanded">
+              <input class="input is-large" type="text" name="search" placeholder="Where did you eat today?" <?php
+                //This uses get to check if it needs to reload a seach term. If a user navigates to page=search manually it will return not input value
+                if (isset($_GET['page'])){
+                  if (isset($_POST['search'])) {
+                    echo "value='".$_POST['search']."'";
+                  }
                 }
-              }
-            ?>>
+              ?>>
+            </div>
           </div>
-        </div>
-
-
-
-        <div>
-          <input class="button is-primary is-large is-outlined level-right" type="submit" name="submit" value="GO!">
-        </div>
+          <div>
+            <input class="button is-primary is-large is-outlined level-right" type="submit" name="submit" value="GO!">
+          </div>
 
       </form>
+      </div>
+      <div class="container column level-right">
+        <?php
+        if (isset($_POST['search'])){
+          $results = new Restaurant;
+          $search = $results->restaurantSearch($_POST['search']);
+          foreach ($search as $result ) {
+            echo "<a href='index.php?page=restaurant&amp;id=".$result->restaurantID."'>".$result->name."</a><p>".$result->description."</p><p>Location: ".$result->location;
+            }
+          }
+        ?>;
+      </div>
     </div>
-  </div>
-</section>
+  </section>
 
 </div>
--- view/results.php
+++ view/results.php
@@ -3,12 +3,21 @@
 $page = new Page;
 $page->header();
 
-//Temporary setup but is functional.
-if (isset($_POST['search'])){
-$results = new Restaurant;
-$search = $results->restaurantSearch($_POST['search']);
-foreach ($search as $result ) {
-  echo "<a href='index.php?page=restaurant&amp;id=".$result->restaurantID."'>".$result->name."</a><p>".$result->description."</p><p>Location: ".$result->location;
-  }
-}
+?>;
+<div class="container column level-right">
+  <?php
+  if (isset($_POST['search'])){
+    $results = new Restaurant;
+    $search = $results->restaurantSearch($_POST['search']);
+    foreach ($search as $result ) {
+      echo "<a href='index.php?page=restaurant&amp;id=".$result->restaurantID."'>".$result->name."</a><p>".$result->description."</p><p>Location: ".$result->location;
+      }
+    }
+  ?>;
+</div>
+
+<?php
+
 $page->footer();
+
+?>;
