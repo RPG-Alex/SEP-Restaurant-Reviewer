6bf669ad8c1f1015158b2cffbb3c194cff7f7e3e
Neildor <superman4kryptonite@hotmail.com>
2019-12-22 23:13:39 +0000

    Styling sliders - more to do with this!!

--- view/all/header.php
+++ view/all/header.php
@@ -7,6 +7,10 @@
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+
+  <!--CUSTOM STYLES-->
+  <link rel="stylesheet" media="all" href="<?php echo './styles.css' ?>" />
+  
   <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 
   <title><?php echo SITENAME; ?></title>
--- view/postReview.php
+++ view/postReview.php
@@ -2,12 +2,33 @@
 <h1>My Review: </h1>
 <form class="" action="<?php echo "index.php?page=restaurant&id=".$_GET['id']; ?>" method='post'>
   <h1>Rate your experience (scale of 1-10)</h1>
-  Quality of Service: <input type="range" min='1' max = '10' name='qos' />
-  Quality of Food: <input type="range" min='1' max = '10' name='qof' />
-  Cleanliness: <input type="range" min='1' max = '10' name='clean' />
-  Speed of Servce: <input type="range" min='1' max = '10' name='spe' />
-  Value: <input type="range" min='1' max = '10' name='val' />
-  Allergy Info Provided? <input type="radio" name="aler" value="true"> Yes | <input type="radio" name="aler" value="false"> No
-  Short Review: <input type="text" name="short" placeholder="Tell us about your experience in under 150 characters" required maxlength="149" size="140"/>
+  <div>
+    Quality of Service: 
+    <input class="scale" type="range" min='1' max = '10' name='qos' />
+  </div>
+  <div>
+    Quality of Food: 
+    <input class="scale" type="range" min='1' max = '10' name='qof' />
+  </div>
+  <div>
+    Cleanliness: 
+    <input class="scale" type="range" min='1' max = '10' name='clean' />
+  </div>
+  <div>
+    Speed of Servce: 
+    <input class="scale" type="range" min='1' max = '10' name='spe' />
+  </div>
+  <div>
+    Value: 
+    <input class="scale" type="range" min='1' max = '10' name='val' />
+  </div>
+  </div>
+    Allergy Info Provided? 
+    <input type="radio" name="aler" value="true"> Yes <input type="radio" name="aler" value="false"> No
+  </div>
+  <div>
+  Short Review: 
+  <input type="text" name="short" placeholder="Tell us about your experience in under 150 characters" required maxlength="149" size="140"/>
+</div>
   <input type="submit" name="Critique!" value="submit">
 </form>
