819a0af5793912dd3e57d807927a96030f8be70c
Neildor <superman4kryptonite@hotmail.com>
2019-12-27 18:29:12 +0000

    changed slider style, added styling to restaurantInfo, and styled Admin login

--- view/admin-login.php
+++ view/admin-login.php
@@ -1,5 +1,18 @@
-<form class="" action="" method="">
-  <input type="text" name="userid" placeholder="username">
-  <input type="password" name="password" placeholder="input password">
-  <input type="submit" name="login" value="login">
-</form>
+<section class="hero is-fullheight-with-navbar is-dark">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title has-text-primary">Administrator Login</h1>
+      <form class="" action="" method="">
+        <div class="field">
+          <input class="admin input" type="text" name="userid" placeholder="username">
+        </div>
+        <div class="field">
+          <input class="admin input" name="password" placeholder="input password">
+        </div>
+        <div class="field">
+          <input class="admin button is-primary" type="submit" name="login" value="login">
+        </div>
+    </form>
+    </div>
+  </div>
+</section>
--- view/restaurantInfo.php
+++ view/restaurantInfo.php
@@ -5,16 +5,18 @@ $details = $restaurant->getRestuarantByID($RID);
 //need to create a catch here. in the event an ID is input that is not valid, sometihng like 'sorry the page you are looking for doesn't exist'
 ?>
     <div class="container hero is-medium is-primary is-fluid">
-        <br/>
-        <h3 class="title has-text-dark"><strong>Restaurant Name:</strong> <?php echo $details->name; ?></h3>
-        <p class="subtitle has-text-dark">
-            <strong class="has-text-dark">description:</strong> <?php echo $details->description; ?>
-        </p>
-        <p class="subtitle has-text-dark">
-            <strong class="has-text-dark">Location:</strong> <?php echo $details->location; ?>
-        </p>
-        <p class="subtitle has-text-dark">
-            <strong class="has-text-dark">Contact:</strong> <?php echo $details->contact; ?>
-        </p>
-        <hr/>
+        <div class="container hero is-medium is-primary is-fluid restaurant-info" style="">
+            <br/>
+            <h1 class="title"><strong>Restaurant Name:</strong> <?php echo $details->name; ?></h1>
+            <h3 class="subtitle has-text-dark">
+                <strong class="has-text-dark">Description:</strong> <?php echo $details->description; ?>
+            </h3>
+            <h3 class="subtitle has-text-dark">
+                <strong class="has-text-dark">Location:</strong> <?php echo $details->location; ?>
+            </h3>
+            <h3 class="subtitle has-text-dark">
+                <strong class="has-text-dark">Contact:</strong> <?php echo $details->contact; ?>
+            </h3>
+            <hr/>
+        </div>
     </div>
