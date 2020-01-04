f948d5142cccc9f608deca4e8516302e205fb680
Neildor <superman4kryptonite@hotmail.com>
2020-01-04 20:11:26 +0000

    Admin View styled

--- view/admin.php
+++ view/admin.php
@@ -1,60 +1,73 @@
 <?php
-if (!isset($_SESSION['logged_in'])) {
-  require_once '../controller/init.php';
-  header('Location: '.URLROOT);
-}
-$reviews = new Admin;
-$unapproved = $reviews->getAllUnapprovedReviews();
-
-if (isset($_POST)) {
-  if (isset($_POST['ACCEPT'])) {
-    $reviews->approveReview($_POST['reviewID']);
-    header('Location: '.URLROOT."/index.php?page=admin");
-  } elseif (isset($_POST['REJECT'])){
-    $reviews->rejectReview($_POST['reviewID']);
-    header('Location: '.URLROOT."/index.php?page=admin");
+  if (!isset($_SESSION['logged_in'])) {
+    require_once '../controller/init.php';
+    header('Location: '.URLROOT);
   }
-}
- ?>
+  $reviews = new Admin;
+  $unapproved = $reviews->getAllUnapprovedReviews();
+
+  if (isset($_POST)) {
+    if (isset($_POST['ACCEPT'])) {
+      $reviews->approveReview($_POST['reviewID']);
+      header('Location: '.URLROOT."/index.php?page=admin");
+    } elseif (isset($_POST['REJECT'])){
+      $reviews->rejectReview($_POST['reviewID']);
+      header('Location: '.URLROOT."/index.php?page=admin");
+    }
+  }
+?>
 
 <!-- PLEASE STYLE ME -->
-Welcome Admin! <br>
-Reviews to be Approved:
-<table border = 1>
-  <tr>
-    <th>Restaurant</th>
-    <th>cost</th>
-    <th>quality of food</th>
-    <th>quality of service</th>
-    <th>cleanliness</th>
-    <th>service speed</th>
-    <th>value</th>
-    <th>allergy info</th>
-    <th>overall</th>
-    <th>Review</th>
-    <th>Approve/Reject</th>
-  </tr>
-  <?php
-  foreach ($unapproved as $pending) {
-    echo "<tr>
-      <td>$pending->name</td>
-      <td>$pending->cost</td>
-      <td>$pending->qfood</td>
-      <td>$pending->qservice</td>
-      <td>$pending->qclean</td>
-      <td>$pending->spservice</td>
-      <td>$pending->value</td>
-      <td>$pending->allergyInfo</td>
-      <td>$pending->overall</td>
-      <td>$pending->shortReview</td>
-      <td>
-        <form class='' action='index.php?page=admin' method='post'>
-        <input type='submit' name='ACCEPT' value='ACCEPT'>
-        <input type='submit' name='REJECT' value='REJECT'>
-        <input type='hidden' name='reviewID' value='$pending->reviewID' >
-        </form>
-      </td>
-    </tr>";
-  }
-   ?>
-</table>
+<section class="hero is-dark is-fullheight-with-navbar is-bold">
+  <div class="container-fluid">
+    <div class="has-text-centered">
+      <h1 class="title is-1">Welcome Admin!</h1>
+      <h3 class="title is-3">Reviews to be Approved:</h3>
+    </div>
+    <div class="container">
+      <table class="table is-striped" style="margin: 0 auto;">
+        <thead>
+          <tr>
+            <th>Restaurant</th>
+            <th>Cost</th>
+            <th>Food quality</th>
+            <th>Service Quality</th>
+            <th>Cleanliness</th>
+            <th>Service speed</th>
+            <th>Value</th>
+            <th>Allergy info</th>
+            <th>Overall</th>
+            <th>Review</th>
+            <th></th>
+            <th></th>
+          </tr>
+        <thead>
+        <?php
+        foreach ($unapproved as $pending) {
+          echo "<tr>
+            <td>$pending->name</td>
+            <td>$pending->cost</td>
+            <td>$pending->qfood</td>
+            <td>$pending->qservice</td>
+            <td>$pending->qclean</td>
+            <td>$pending->spservice</td>
+            <td>$pending->value</td>
+            <td>$pending->allergyInfo</td>
+            <td>$pending->overall</td>
+            <td>$pending->shortReview</td>
+              <form class='' action='index.php?page=admin' method='post'>
+              <td>
+              <input class='button is-success' type='submit' name='ACCEPT' value='ACCEPT'>
+              </td>
+              <td>
+              <input class='button is-danger' type='submit' name='REJECT' value='REJECT'>
+              </td>
+              <input type='hidden' name='reviewID' value='$pending->reviewID' >
+              </form>
+          </tr>";
+        }
+        ?>
+      </table>
+    </div>  
+  </div>
+</section>
